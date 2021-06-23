e1d02ccf664e97705dc38134e63a8ea7d9904fb2
DeepikaMuthu <86340971+DeepikaMuthu@users.noreply.github.com>
2021-06-23 20:20:49 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,28 @@
+print('***Indexing***')
+days='python language'
+print(days[1])
+print('***slicing***')
+str='core python'
+print(str[0:5:1])
+print('***length of string***')
+str='core python'
+n=len(str)
+print(n)
+print('***concatenation***')
+s1='core'
+s2='python'
+s3=s1+s2
+print(s3)
+print('***casefold***')
+text='thirty days thirty challenge'
+x=text.capitalize()
+print(x)
+text='thirty days'
+x=text.isalpha()
+print(x)
+text='thirty days'
+x=text.isalnum()
+print(x)
+
+
+
